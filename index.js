1b84951404c41a13658ef89f830f8927db1a3df3
nikitasenko <nikita.senko@mifort.org>
2018-06-26 18:06:44 +0300

    add end-point

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const {Botact} = require('botact');
 
 const app = express();
-
 const bot = new Botact({
-    token: '6b4ef7576050749177c4839cb809a3edfa1d385602787db783194866821eb70e4b1e2cc6b4bdf86ffb870',
-    confirmation: 'af2d3308'
+    confirmation: 'af2d3308',
+    token: '6b4ef7576050749177c4839cb809a3edfa1d385602787db783194866821eb70e4b1e2cc6b4bdf86ffb870'
 });
 
-bot.on(function (ctx) {
-    console.log(ctx.body);
 
-    //ctx.reply('12321');
+bot.on(  (ctx) => {
+     ctx.reply('Вы написали: ' +ctx.body);
+    ctx.reply('А я отвечу: люблю настю)))))))))) ');
 });
+bot.command('start', ({ reply }) => reply('This is start!'));
 
 app.use(bodyParser.json());
-
 app.post('/', bot.listen);
-
-
-app.listen(80);
+app.listen(8080);
